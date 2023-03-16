1c398a1e0e1ebe03ee30fa7ce61b4a7f0bc28113
unknown <teukudr@gmail.com>
2023-03-16 21:08:06 +0700

    initial commit

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -4,16 +4,12 @@ function filterCarByAvailability(cars) {
 
   // Tempat penampungan hasil
   const result = [];
-  //let available = 0;
 
   // Tulis code-mu disini
 
     for(let i = 0; i < cars.length; i++){
     if (cars[i].available === true) {
 
-    //  result[available] = cars[i]
-    //  available++
-  
     result.push(cars[i])
     }
 
--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -7,6 +7,15 @@ function sortCarByYearDescendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  for(let i=0; i<result.length; i++){
+    for(let j=0; j< result.length-1; j++){
+      if (result[j].year < result[j+1].year){
+        let temp = result[j];
+        result[j] = result [j+1];
+        result[j+1] = temp;
+      }
+    }
+  }
 
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
